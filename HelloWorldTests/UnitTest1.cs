462e12161db7076b5849bbc1e026e01ba7a2ab46
Kostya.Mitskevich <kostettt1@gmail.com>
2017-12-30 12:26:58 +0300

    commit8

--- HelloWorldTests/UnitTest1.cs
+++ HelloWorldTests/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace HelloWorldTests
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual("Hello World2", Program.CreateMessage());
+            Assert.AreEqual("Hello World", Program.CreateMessage());
         }
     }
 }
